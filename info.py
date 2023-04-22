3ef76ec93d80a3255db7f2c28c53ef4688e2375e
saurabh8753 <130946713+saurabh8753@users.noreply.github.com>
2023-04-22 09:36:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 20559700
 API_HASH     = "9fb86922c6cba4d6a38b664a114d9bd0"
 BOT_TOKEN    = "6299115370:AAGIhhbxqQM_avT910CeRmA0m1xTO2x3h-U"
 SESSION      = "1BVtsOKwBuxluRFX8WVPqBArbK-9AehcqBhOOYJ4HKIzmN-rU-IRaTQgoBlkGeHSLHVvLAvkLPnMmxGVA0lzkhW52AFpVZfy_Jd8u-a85GLXIq3L7cYey5Ja_iUjDiTWVHV5jmdrzvIApXrZ9SwqTnbvrQR4X6EaR_CFzXrwjLD-tvNkUqAEiCxgIHb3xcB4d4HN79-_KO17WiZH8hgbhUsGXWvbjEe4M5EgGM1bSv-cge8eoM5pBSjVdVn0mV6LEmoRd0zrI8MYrR31QtZ3IKJhdMB7vjkSESOw51sJ8cArbQ5D8MbPlIZz9NElTaDyXbPB3JZINrW1aig-jK6XEwD2ObpHto_I="
-DATABASE_URI = "1840733451"
+DATABASE_URI = "mongodb+srv://yaartunah:<password>@cluster0.ryn6fnj.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1972473900,
 ADMIN        = 6165685589
 CHANNEL      = "@backup0990"
